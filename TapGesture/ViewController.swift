2053572dcfe67808c47a0664c73cce2d844fc648
Sanghun <po4865+github_sanghun@gmail.com>
2021-07-27 14:57:10 +0900

    added tab gesture recognizer with delegate

--- TapGesture/ViewController.swift
+++ TapGesture/ViewController.swift
@@ -7,11 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer()
+        tapGesture.delegate = self
+        
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        self.view.endEditing(true)
+        return true
     }
 
 
